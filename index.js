888e4023e7b1475d044e7249106098027d17dcce
Andrei Sergo <a.sergoucht@gmail.com>
2018-11-25 23:43:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ function nowServing(line) {
   if (line.length === 0) {
     return "There is nobody waiting to be served!"
   } else {
-  return "Currently serving " + line[0] + "."
-  line.shift()
+    return "Currently serving " + line[0] + "."
+    line.shift()
   }
 }
 
